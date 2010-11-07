ae9852d4442ecf3f257870617db74f17577a4dc6
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2010-11-07 11:54:34 +0100

    Explicit exports. Documentation.

--- Web/Zwaluw.hs
+++ Web/Zwaluw.hs
@@ -1,7 +1,20 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Web.Zwaluw where
+module Web.Zwaluw (
+    -- * Types
+    P, (:-)(..), (<>),
+    
+    -- * Running routers
+    parse, unparse,
+    
+    -- * Constructing routers
+    -- | The @constrN@ functions are helper functions to lift constructors of
+    -- datatypes to routers. Their first argument is the constructor; their
+    -- second argument is a (partial) destructor.
+    constr0, constr1, constr2,
+    int, slash, lit
+  ) where
 
 import Prelude hiding ((.), id)
 import Control.Monad
@@ -12,6 +25,7 @@ import Data.Monoid
 infixr 8 <>
 infixr 8 :-
 
+-- | Infix operator for 'mappend'.
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
 
@@ -60,11 +74,11 @@ nil = constr0 [] $ \x -> do [] <- Just x; Just ()
 cons :: P (a :- [a] :- r) ([a] :- r)
 cons = constr2 (:) $ \x -> do a:as <- Just x; Just (a, as)
 
-many :: Eq a => (forall r. P r (a :- r)) -> P r ([a] :- r)
-many p = nil <> many1 p
+-- many :: Eq a => (forall r. P r (a :- r)) -> P r ([a] :- r)
+-- many p = nil <> many1 p
 
-many1 :: Eq a => (forall r. P r (a :- r)) -> P r ([a] :- r)
-many1 p = cons . p . many p
+-- many1 :: Eq a => (forall r. P r (a :- r)) -> P r ([a] :- r)
+-- many1 p = cons . p . many p
 
 satisfy :: (Char -> Bool) -> P r (Char :- r)
 satisfy p = P
@@ -82,14 +96,23 @@ digitChar = satisfy (\c -> c >= '0' && c <= '9')
 digit :: P r (Int :- r)
 digit = maph (head . show) (read . (:[])) digitChar
 
-int :: P r (Int :- r)
--- int = maph show read $ many1 digitChar
-int = digit
 
+-- | Insert a constant string.
 lit :: String -> P r r
 lit l = P
   (\b -> return (b, l))
   (\s -> let (s1, s2) = splitAt (length l) s in if s1 == l then return (id, s2) else mzero)
+
+-- | Insert a slash.
+slash :: P r r
+slash = lit "/"
+
+-- | Insert any integer.
+int :: P r (Int :- r)
+-- int = maph show read $ many1 digitChar
+int = digit
+
+
 
 push :: Eq h => h -> P r (h :- r)
 push h = P 
@@ -105,19 +128,28 @@ right = constr1 Right $ \x -> do Right b <- Just x; return b
 eitherP :: P r (a :- r) -> P r (b :- r) -> P r (Either a b :- r)
 eitherP l r = left . l <> right . r
 
-slash :: P r r
-slash = lit "/"
-
+-- | For example:
+-- 
+-- > nil :: P r ([a] :- r)
+-- > nil = constr0 [] $ \x -> do [] <- Just x; Just ()
 constr0 :: o -> (o -> Maybe ()) -> P r (o :- r)
 constr0 c d = P 
   (\(a :- t) -> maybe mzero (\_ -> return (t, "")) (d a))
   (\s -> return ((c :-), s))
 
+-- | For example:
+--
+-- > left :: P (a :- r) (Either a b :- r)
+-- > left = constr1 Left $ \x -> do Left a <- Just x; return a
 constr1 :: (a -> o) -> (o -> Maybe a) -> P (a :- r) (o :- r)
 constr1 c d = P
   (\(a :- t) -> maybe mzero (\a -> return (a :- t, "")) (d a))
   (\s -> return (\(a :- t) -> c a :- t, s))
 
+-- | For example:
+--
+-- > cons :: P (a :- [a] :- r) ([a] :- r)
+-- > cons = constr2 (:) $ \x -> do a:as <- Just x; Just (a, as)
 constr2 :: (a -> b -> o) -> (o -> Maybe (a, b)) -> P (a :- b :- r) (o :- r)
 constr2 c d = P
   (\(a :- t) -> maybe mzero (\(a, b) -> return (a :- b :- t, "")) (d a))
