34ded4eab968292ebc103f33f8014c54657e6e18
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2010-11-07 11:31:06 +0100

    Inlined ConstrN type constructors Now consistently using polymorphic type parameter r

--- Example.hs
+++ Example.hs
@@ -12,19 +12,19 @@ data Sitemap
    | CaseDetail Int
    deriving (Eq, Show)
 
-home :: Constr0 Sitemap
+home :: P r (Sitemap :- r)
 home = constr0 Home $ \a -> do Home <- Just a; Just ()
 
-caseOverview :: Constr0 Sitemap
+caseOverview :: P r (Sitemap :- r)
 caseOverview = constr0 CaseOverview $ \a -> do CaseOverview <- Just a; Just ()
 
-caseDetail :: Constr1 Sitemap Int
+caseDetail :: P (Int :- r) (Sitemap :- r)
 caseDetail = constr1 CaseDetail $ \a -> do CaseDetail i <- Just a; Just i
 
-range :: Constr2 Sitemap Int Int
+range :: P (Int :- Int :- r) (Sitemap :- r)
 range = constr2 Range $ \a -> do Range l u <- Just a; Just (l, u)
 
-url :: P t (Sitemap :- t)
+url :: P r (Sitemap :- r)
 url = 
   slash . 
   (  home . lit "home"
--- Web/Zwaluw.hs
+++ Web/Zwaluw.hs
@@ -21,7 +21,7 @@ data P a b = P
 
 data a :- b = a :- b deriving (Eq, Show)
 
-xmap :: (b -> a) -> (a -> b) -> P t a -> P t b
+xmap :: (b -> a) -> (a -> b) -> P r a -> P r b
 xmap f g (P s p) = P (s . f) ((fmap . liftM . first . fmap) g p)
   
 instance Category (P) where
@@ -54,10 +54,10 @@ maph f g = xmap (\(h :- t) -> f h :- t) (\(h :- t) -> g h :- t)
 opt :: Eq a => a -> P r (a :- r) -> P r (a :- r)
 opt a p = p <> push a
 
-nil :: Constr0 [a]
+nil :: P r ([a] :- r)
 nil = constr0 [] $ \x -> do [] <- Just x; Just ()
 
-cons :: Constr2 [a] a [a]
+cons :: P (a :- [a] :- r) ([a] :- r)
 cons = constr2 (:) $ \x -> do a:as <- Just x; Just (a, as)
 
 many :: Eq a => (forall r. P r (a :- r)) -> P r ([a] :- r)
@@ -91,37 +91,34 @@ lit l = P
   (\b -> return (b, l))
   (\s -> let (s1, s2) = splitAt (length l) s in if s1 == l then return (id, s2) else mzero)
 
-push :: Eq h => h -> P t (h :- t)
+push :: Eq h => h -> P r (h :- r)
 push h = P 
   (\(h' :- t) -> do guard (h == h'); return (t, ""))
   (\s -> return ((h :-), s))
 
-left :: Constr1 (Either a b) a
+left :: P (a :- r) (Either a b :- r)
 left = constr1 Left $ \x -> do Left a <- Just x; return a
 
-right :: Constr1 (Either a b) b
+right :: P (b :- r) (Either a b :- r)
 right = constr1 Right $ \x -> do Right b <- Just x; return b
  
-eitherP :: P t (a :- t) -> P t (b :- t) -> P t (Either a b :- t)
+eitherP :: P r (a :- r) -> P r (b :- r) -> P r (Either a b :- r)
 eitherP l r = left . l <> right . r
 
 slash :: P r r
 slash = lit "/"
 
-type Constr0 o = forall t. P t (o :- t)
-constr0 :: o -> (o -> Maybe ()) -> Constr0 o
+constr0 :: o -> (o -> Maybe ()) -> P r (o :- r)
 constr0 c d = P 
   (\(a :- t) -> maybe mzero (\_ -> return (t, "")) (d a))
   (\s -> return ((c :-), s))
 
-type Constr1 o a = forall t. P (a :- t) (o :- t) 
-constr1 :: (a -> o) -> (o -> Maybe a) -> Constr1 o a
+constr1 :: (a -> o) -> (o -> Maybe a) -> P (a :- r) (o :- r)
 constr1 c d = P
   (\(a :- t) -> maybe mzero (\a -> return (a :- t, "")) (d a))
   (\s -> return (\(a :- t) -> c a :- t, s))
 
-type Constr2 o a b = forall t. P (a :- b :- t) (o :- t) 
-constr2 :: (a -> b -> o) -> (o -> Maybe (a, b)) -> Constr2 o a b
+constr2 :: (a -> b -> o) -> (o -> Maybe (a, b)) -> P (a :- b :- r) (o :- r)
 constr2 c d = P
   (\(a :- t) -> maybe mzero (\(a, b) -> return (a :- b :- t, "")) (d a))
   (\s -> return (\(a :- b :- t) -> c a b :- t, s))
